e0e8da80611a4ef96d81f4b45493791de14b1d50
Jack Goodall <jack.goodall@epitech.eu>
2023-10-07 16:35:30 +0200

    add delete and edit features

--- server.go
+++ server.go
@@ -13,27 +13,45 @@ type Todo struct {
 	Done  bool
 }
 
-func main() {
-	fmt.Println("---------------")
-	fmt.Println("hello world")
+func getIdFromPath(w http.ResponseWriter, r *http.Request, path string) (int, error) {
+	var slug int
+	_, err := fmt.Sscanf(r.URL.Path, path, &slug)
+	if err != nil {
+		return -1, err
+	}
+	return slug, nil
+}
 
+func getTmpl(name string) *template.Template {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	switch name {
+	case "": // root
+		return tmpl
+	case "todo":
+		return tmpl.Lookup("todo")
+	case "edit":
+		return tmpl.Lookup("edit")
+	default:
+		return nil
+	}
+}
+
+func methodNotAllowed(w http.ResponseWriter) {
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+func main() {
 	todos := []Todo{
 		{1, "Task 1", true},
 		{2, "Task 2", true},
 		{3, "Task 3", false},
-	}
-	getRootTmpl := func() *template.Template {
-		return template.Must(template.ParseFiles("index.html"))
-	}
-	getTodoTmpl := func() *template.Template {
-		return getRootTmpl().Lookup("todo")
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		todos := map[string][]Todo{
 			"Todos": todos,
 		}
-		getRootTmpl().Execute(w, todos)
+		getTmpl("").Execute(w, todos)
 	})
 
 	http.HandleFunc("/add-todo", func(w http.ResponseWriter, r *http.Request) {
@@ -41,21 +59,95 @@ func main() {
 
 		todo := Todo{4, title, false}
 		todos = append(todos, todo)
-		getTodoTmpl().Execute(w, todo)
+		getTmpl("todo").Execute(w, todo)
 	})
 
+	toggleTodo := func(w http.ResponseWriter, r *http.Request, id int) {
+		var todo Todo
+		for i := 0; i < len(todos); i++ {
+			if todos[i].ID == id {
+				todos[i].Done = !todos[i].Done
+				todo = todos[i]
+
+				getTmpl("todo").Execute(w, todo)
+				return
+			}
+		}
+		http.NotFound(w, r)
+	}
+
 	http.HandleFunc("/toggle-todo/", func(w http.ResponseWriter, r *http.Request) {
-		var slug int
-		_, err := fmt.Sscanf(r.URL.Path, "/toggle-todo/%d", &slug)
+		if r.Method != http.MethodPost {
+			methodNotAllowed(w)
+			return
+		}
+
+		id, err := getIdFromPath(w, r, "/toggle-todo/%d")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
+		toggleTodo(w, r, id)
+	})
+
+	deleteTodo := func(w http.ResponseWriter, r *http.Request, id int) {
+		for i := 0; i < len(todos); i++ {
+			if todos[i].ID == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+		}
+		http.NotFound(w, r)
+	}
+
+	updateTitle := func(w http.ResponseWriter, r *http.Request, id int, title string) {
+		for i := 0; i < len(todos); i++ {
+			if todos[i].ID == id {
+				todos[i].Title = title
+				getTmpl("todo").Execute(w, todos[i])
+				return
+			}
+		}
+		http.NotFound(w, r)
+	}
+
+	http.HandleFunc("/todo/", func(w http.ResponseWriter, r *http.Request) {
+		id, err := getIdFromPath(w, r, "/todo/%d")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		switch r.Method {
+		case http.MethodDelete:
+			deleteTodo(w, r, id)
+			break
+		case http.MethodPut:
+			title := r.PostFormValue("title")
+			updateTitle(w, r, id, title)
+			break
+		default:
+			methodNotAllowed(w)
+			break
+		}
+	})
+
+	http.HandleFunc("/edit/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			methodNotAllowed(w)
+			return
+		}
+
+		id, err := getIdFromPath(w, r, "/edit/%d")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 		var todo Todo
 		for i := 0; i < len(todos); i++ {
-			if todos[i].ID == slug {
-				todos[i].Done = !todos[i].Done
+			if todos[i].ID == id {
 				todo = todos[i]
 			}
 		}
@@ -64,8 +156,10 @@ func main() {
 			return
 		}
 
-		getTodoTmpl().Execute(w, todo)
+		getTmpl("edit").Execute(w, todo)
 	})
 
+	fmt.Println("---------------")
+	fmt.Println("Server started at http://localhost:4242")
 	log.Fatal(http.ListenAndServe(":4242", nil))
 }
